2c12c99cf6f392abb226f42cd487ab0df385705d
pepsivstim <pepsivstim@gmail.com>
2022-10-20 00:04:29 -0400

    hamburger functionality

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,8 +9,8 @@ function Header() {
         <a href="/" className="logo">TIMOTHY CHU</a>
       </div>
       <div className="header-right">
-        <Button name={"About"} href="/about"/>
-        <Button name={"Resume"} href="/resume"/>
+        <Button name="About" href="/about"/>
+        <Button name="Resume" href="/resume"/>
         <div class="dropdown">
           <h1 class="dropbutton" href='/'>Media</h1>
           <div class="dropdown-content">
@@ -32,6 +32,17 @@ function Header() {
           <div className='hamburger-line'></div>
           <div className='hamburger-line'></div>
           <div className='hamburger-line'></div>
+          <div class="hamburger-content">
+            <Button name="About" href="/about"/>
+            <Button name="Resume" href="/resume"/>
+            <Button name="Photo" href="/photos"/>
+            <Button name="Art" href="/art"/>
+            <Button name="Video" href="/video"/>
+            <Button name="Running" href="/art"/>
+            <Button name="Aquariums" href="/video"/>
+            <Button name="Music" href="/music"/>
+          </div>
+          
       </div>
     </div>
   );
