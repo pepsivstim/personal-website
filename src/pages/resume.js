41837d044da8ce78928cbf1c81b51d0391112253
pepsivstim <pepsivstim@gmail.com>
2022-10-23 00:48:09 -0400

    updated resume page

--- src/pages/resume.js
+++ src/pages/resume.js
@@ -6,8 +6,8 @@ import '../css/resume.css'
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
-
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
 function Resume() {
   const downloadSVG = <svg width="21" height="21" viewBox="0 0 24 24" fill="none" stroke="#8899a4" stroke-width="2" stroke-linecap="square" stroke-linejoin="arcs">
                         <path d="M3 17v4c0 1.1.9 2 2 2h14a2 2 0 0 0 2-2v-4M17 12l-5 5-5-5M12 14.8V2.5"></path>
@@ -15,8 +15,8 @@ function Resume() {
   return (
     <div className="page-container">
       <Header /> 
-      <main>
-        <h1 className='resume-text'>Download Resume <a href='Timothy_Chu_Resume.pdf' target='_blank' rel='noopener noreferrer' className='resume-text'> {downloadSVG}</a></h1>
+      <main className='resume-main'>
+        <a className='resume-text' href='Timothy_Chu_Resume.pdf' target='_blank' rel='noopener noreferrer'>Download Resume {downloadSVG}</a>
         <Document file='Timothy_Chu_Resume.pdf'>
           <Page pageNumber={1}/>
         </Document>
